8b490c8cdd513c601ece623e8afba316a221bb3b
Vijay Shukla <vks885353@gmail.com>
2026-01-20 09:09:24 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@r

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -57,7 +57,7 @@ const CardDescription = React.forwardRef<
 CardDescription.displayName = "CardDescription"
 
 const CardContent = React.forwardRef<
-_SIDE HTMLDivElement,
+  HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
